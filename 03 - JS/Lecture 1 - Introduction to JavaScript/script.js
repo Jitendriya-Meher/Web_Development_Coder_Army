8894d5d599f1d3e21b890bb507229882cb291e7a
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-11-21 18:08:34 +0530

    js day 1

---
+++ 03 - JS/Lecture 1 - Introduction to JavaScript/script.js
@@ -0,0 +1,21 @@
+console.log("Hii JS");
+
+// variables
+
+// let , const , var
+// var old
+
+let num = 10;
+console.log("num",num);
+
+num = 20;
+console.log('num',num);
+
+let str = "Jitendriya";
+console.log("str",str);
+
+const id = 20;
+console.log("id",id);
+
+// id = 30; // error
+
