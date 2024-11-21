52f375776f6d333d0e257909ce44cd119e56b7ec
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-11-21 20:02:08 +0530

    js day 2

---
+++ 03 - JS/Lecture 02 - Datatypes in JS Part - 1/index.js
@@ -0,0 +1,40 @@
+// premitive data type 
+// Number
+// String
+// Boolean
+// null
+// undefined
+// Symbol
+
+let num = 13.45;
+console.log(num);
+console.log(typeof(num));
+
+let str = "amit";
+console.log(str);
+console.log(typeof(str));
+
+let bool = false;
+console.log(bool);
+console.log(typeof(bool));
+
+let und;
+console.log(und);
+console.log(typeof(und));
+
+let nul = null;
+console.log(nul);
+console.log(typeof(nul));
+
+// bigint 
+let a = 12345678901234567891234567890n;
+console.log(a);
+console.log(typeof(a));
+
+
+console.log("MAX_SAFE_INTEGER = ",Number.MAX_SAFE_INTEGER);
+
+console.log("MIN_SAFE_INTEGER = ",Number.MIN_SAFE_INTEGER);
+
+console.log("MAX_VALUE = ",Number.MAX_VALUE);
+console.log("MIN_VALUE = ",Number.MIN_VALUE);
